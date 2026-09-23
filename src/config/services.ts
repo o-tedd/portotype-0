64b7fe72dc0c8e7618356e19f8ae8a0124ca0b38
Patrick Theodoro "L." Silva <79939301+o-tedd@users.noreply.github.com>
2026-09-23 12:13:23 -0300

    feat(tasks): register task repository and service

--- src/config/services.ts
+++ src/config/services.ts
@@ -1,12 +1,17 @@
 import { auth, db } from "./firebase";
 import { FirebaseAuthRepository } from "../repositories/firebase/FirebaseAuthRepository";
+import { FirebaseTaskRepository } from "../repositories/firebase/FirebaseTaskRepository";
 import { FirebaseUserRepository } from "../repositories/firebase/FirebaseUserRepository";
 import { AuthService } from "../services/AuthService";
+import { TaskService } from "../services/TaskService";
 
 const authRepository = new FirebaseAuthRepository(auth);
 const userRepository = new FirebaseUserRepository(db);
+const taskRepository = new FirebaseTaskRepository(db);
 
 export const authService = new AuthService(
   authRepository,
   userRepository,
 );
+
+export const taskService = new TaskService(taskRepository);
